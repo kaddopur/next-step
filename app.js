f5f9e380e5058dc959789eda02dbf8e0d6950ad4
Jason Huang <cjhuang@yahoo-inc.com>
2016-01-12 15:48:13 +0800

    init project

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+let app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
